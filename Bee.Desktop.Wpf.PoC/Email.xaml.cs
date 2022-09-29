638ff3712fc659729c6cb02ccf287300cc2a9c4f
Javier Rivera Zavala <107950410+kyojrz@users.noreply.github.com>
2022-09-29 16:27:32 -0700

    changes for port and server ip

---
+++ Bee.Desktop.Wpf.PoC/Email.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Bee.Desktop.Wpf.PoC
+{
+    /// <summary>
+    /// Interaction logic for Email.xaml
+    /// </summary>
+    public partial class Email : Page
+    {
+        public Email()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bee.Desktop.Wpf.PoC/Port.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Windows.Controls;
+
+namespace Bee.Desktop.Wpf.PoC
+{
+    /// <summary>
+    /// Interaction logic for Email.xaml
+    /// </summary>
+    public partial class Port : Page
+    {
+        public Port()
+        {
+            InitializeComponent();
+        }
+    }
+}

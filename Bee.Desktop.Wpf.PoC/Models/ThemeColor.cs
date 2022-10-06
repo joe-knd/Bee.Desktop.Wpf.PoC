e570a1cce15b3b27ddca6f5fecf16feda92c8b67
Jose Valencia <joe.k.nd@outlook.com>
2022-10-06 16:12:44 -0700

    added themes

---
+++ Bee.Desktop.Wpf.PoC/Models/ThemeColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bee.Desktop.Wpf.PoC.Models
+{
+    public class ThemeColor
+    {
+        public string? Name { get; set; }
+    }
+}
--- Bee.Desktop.Wpf.PoC/ViewModels/MainViewModel.cs
+++ Bee.Desktop.Wpf.PoC/ViewModels/MainViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using Bee.Desktop.Wpf.PoC.Messenger;
+using Bee.Desktop.Wpf.PoC.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using ControlzEx.Theming;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
 
 namespace Bee.Desktop.Wpf.PoC.Messenger
 {
@@ -11,15 +15,34 @@ namespace Bee.Desktop.Wpf.PoC.Messenger
         [ObservableProperty]
         private BaseViewModel? currentViewModel;
 
+        private ThemeColor selectedTheme;
+
+        [ObservableProperty]
+        private List<ThemeColor>? themes;
+
         private bool canNavigateNext;
         private bool canNavigatePrevious;
         private BaseViewModel? NextViewModel;
         private BaseViewModel? PreviousViewModel;
-
         public NavigationReceiver Receiver { get; } = new NavigationReceiver();
-
         public MainViewModel()
         {
+            Themes ??= new List<ThemeColor>
+                {
+                    new ThemeColor{ Name = "Light.Blue"},
+                    new ThemeColor{ Name = "Light.Red"},
+                    new ThemeColor{ Name = "Light.Crimson"},
+                    new ThemeColor{ Name = "Light.Teal"},
+                    new ThemeColor{ Name = "Light.Steel"},
+                    new ThemeColor{ Name = "Light.Brown"},
+                    new ThemeColor{ Name = "Dark.Blue"},
+                    new ThemeColor{ Name = "Dark.Red"},
+                    new ThemeColor{ Name = "Dark.Crimson"},
+                    new ThemeColor{ Name = "Dark.Teal"},
+                    new ThemeColor{ Name = "Dark.Steel"},
+                    new ThemeColor{ Name = "Dark.Brown"},
+                };
+            
             CurrentViewModel = new AuthorizeViewModel();
             
             // Register a message in some module
@@ -54,7 +77,17 @@ namespace Bee.Desktop.Wpf.PoC.Messenger
                 NavigatePreviousCommand.NotifyCanExecuteChanged();
             });
 
-            this.ValidateAllProperties();
+            ValidateAllProperties();
+        }
+
+        public ThemeColor SelectedTheme
+        {
+            get => selectedTheme;
+            set
+            {
+                ThemeManager.Current.ChangeTheme(App.Current, value.Name);
+                SetProperty(ref selectedTheme, value);
+            }
         }
 
 
